7968c5c0e35c1a4e31346538b767daea9b410528
linberon <linberon@gmail.com>
2018-04-18 22:01:41 +0100

    data file

---
+++ data.js
@@ -0,0 +1,9 @@
+// Data for the markers consisting of a name, a LatLng and a zIndex for the
+// order in which these markers should display on top of each other.
+let markers = [
+	["Fish", -1.878, -78.178, 4, "this is the content for the Fish"],
+	["Monkey", 41, 8, 5, "this is the content for the Monkey"],
+	["Bird", 43, -76, 3, "this is the content for the Bird"],
+	["Bear", -3, 14, 2, "this is the content for the Bear"],
+	["Turtle", 47.567, 19.169, 1, "this is the content for the Turtle"]
+];
