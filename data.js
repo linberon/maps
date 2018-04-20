b6e2c909cd43225bfddcbf517f20fe511839174f
linberon <linberon@gmail.com>
2018-04-20 18:59:55 +0100

    infowindow

--- data.js
+++ data.js
@@ -1,46 +1,43 @@
-// Data for the markers consisting of a name, a LatLng and a zIndex for the
-// order in which these markers should display on top of each other.
-
 let markers = [
-  {
-    name: "Fish",
-    lat: -1.878,
-    lng: -78.178,
-    description: "this is the content for the Fish",
-    otherData: "Hi",
-    image: "/img/fish.jpg"
-  },
-  {
-    name: "Monkey",
-    lat: 41,
-    lng: 8,
-    description: "this is the content for the Monkey",
-    phrase: "Monkey says Hi",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Macaca_sinica_-_01.jpg/440px-Macaca_sinica_-_01.jpg"
-  },
-  {
-    name: "Bird",
-    lat: 43,
-    lng: -76,
-    description: "this is the content for the Bird",
-    otherData: "Hi",
-    image: ""
-  },
-  {
-    name: "Bear",
-    lat: -3,
-    lng: 14,
-    description: "this is the content for the Bear",
-    otherData: "Hi",
-    image: ""
-  },
-  {
-    name: "Turtle",
-    lat: 47.567,
-    lng: 19.169,
-    description: "this is the content for the Turtle",
-    otherData: "Hi",
-    image: ""
-  }
+	{
+		name: "Fish",
+		lat: -1.878,
+		lng: -78.178,
+		description: "this is the content for the Fish",
+		otherData: "Hi",
+		image: "/img/fish.jpg"
+	},
+	{
+		name: "Monkey",
+		lat: 41,
+		lng: 8,
+		description: "this is the content for the Monkey",
+		phrase: "Monkey says Hi",
+		image:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Macaca_sinica_-_01.jpg/440px-Macaca_sinica_-_01.jpg"
+	},
+	{
+		name: "Bird",
+		lat: 43,
+		lng: -76,
+		description: "this is the content for the Bird",
+		otherData: "Hi",
+		image: ""
+	},
+	{
+		name: "Bear",
+		lat: -3,
+		lng: 14,
+		description: "this is the content for the Bear",
+		otherData: "Hi",
+		image: ""
+	},
+	{
+		name: "Turtle",
+		lat: 47.567,
+		lng: 19.169,
+		description: "this is the content for the Turtle",
+		otherData: "Hi",
+		image: ""
+	}
 ];
