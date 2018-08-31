75be017a59848c5491792f5cb17833b2a6bd4941
linberon <linberon@gmail.com>
2018-09-01 00:20:28 +0100

    update content

--- data.js
+++ data.js
@@ -12,7 +12,7 @@ let markers = [
 		name: "Hawaiian Crow",
 		lat: 19.57,
 		lng: -155.61,
-		//status: "Extinct in the wild",
+		//status: "Extinct in the Wild",
 		description: "Be patient, I'm still working on it",
 		//threats: "",
 		image: "imgs/no_img.jpg"
@@ -21,7 +21,7 @@ let markers = [
 		name: "Blue Poison Dart Frog",
 		lat: 3.30,
 		lng: -56.96,
-		status: "Least concern",
+		status: "Least Concern",
 		description: "Also known as Dyeing Poison Frog, this brilliantly coloured amphibian makes a popular exotic pet, and is found in zoos all over the world. They are easily kept and bred in captivity, however wild populations are extremely vulnerable, as they are exposed to illegal collection and environmental factors. The frog's bright colour is a warning to predators. Their skin secretes a very potent toxin, that comes from the insects they eat. In captivity their diet don't contain the same compounds, therefore they cannot produce poison. Only captive-bred frogs should be kept as pets, to protect wild populations.",
 		threats: "Illegal pet trade, habitat loss.",
 		image: "imgs/Blue_poison_frog_sm.jpg"
@@ -39,7 +39,7 @@ let markers = [
 		name: "Red Wolf",
 		lat: 35.67,
 		lng: -76.48,
-		status: "Critically endangered",
+		status: "Critically Endangered",
 		description: "The wild population of Red Wolf is a result of reintroduction projects. The species were extinct in the wild in 1980, and were reintroduced years later in North Carolina. The area was selected because of the absence of Coyotes, as Red Wolf x Coyote hybridization is one of the main issues that led this species to demise. Scientific research started only when numbers were already low, so it is only assumed that their favoured natural habitat types were wetlands and swamps, remote from human activity, although they can thrive in a variety of settings. Captive breeding and reintroduction programmes are still ongoing. Currently there are more wolves known to be living in captivity than in the wild.",
 		threats: "Hybridization with coyotes, human induced deaths. Hybrids are sterilized to protect genetic integrity of the species.",
 		image: "imgs/Red_wolf_sm.jpg"
